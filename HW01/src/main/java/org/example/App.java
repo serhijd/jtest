e144517ef641bb90bab1a8134c62d600a8bfd796
serhijd <sdolnyy@gmail.com>
2021-09-13 20:47:58 +0200

    HW01 - 1st commit

---
+++ HW01/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //1st commit
+        System.out.println( "Hello World!" );
+    }
+}
